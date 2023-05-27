f5cd8678543b6d1c80a5151c7568a47e0947d8ed
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-05-27 15:44:37 +0100

    added logic to exit trades based on stop loss and take profit hits

--- backtest.py
+++ backtest.py
@@ -18,6 +18,11 @@ class BackTester:
         self.last_row = None
 
     def _enter_long(self, row: pd.Series):
+        """
+        Enters Long Trade
+        :param row: Pandas row where the Entry Signal was generated. Must contain `close` column
+        :return: None
+        """
         if self.balance >= self.stake_amount:
             current_price = row["close"]
             amount_to_buy = self.stake_amount / current_price
@@ -25,20 +30,49 @@ class BackTester:
             self.balance -= self.stake_amount
             self.total_trades += 1
 
-    def _exit_trade(self, current_row: pd.Series, trade: dict):
+    def _exit_trade(self, current_row: pd.Series, trade: dict, is_sl_tp: bool = False):
+        """
+        Exits Single Trade
+        :param current_row: Pandas row where the exit signal was generated. Must contain `close` column.
+        :param trade: The trade to be exited. Contains `amount` bought and the buying `price`.
+        :param is_sl_tp: Tells the backtester to only exit trades where stop_loss or take_profit has been hit.
+        :return: None
+        """
         if trade["exited"]:
             return
         position_value = trade["amount"] * current_row["close"]
         if position_value < self.stake_amount:
-            self.losses.append(self.stake_amount - position_value)
+            if not is_sl_tp:
+                self.losses.append(self.stake_amount - position_value)
+            else:
+                loss = self.stake_amount - position_value
+                pct_loss = loss / self.stake_amount
+                if pct_loss >= self.stop_loss:
+                    self.losses.append(self.stake_amount - position_value)
+                else:
+                    return
         if position_value > self.stake_amount:
-            self.wins.append(position_value - self.stake_amount)
+            if not is_sl_tp:
+                self.wins.append(position_value - self.stake_amount)
+            else:
+                gain = self.stake_amount - position_value
+                pct_gain = gain / self.stake_amount
+                if pct_gain >= self.take_profit:
+                    self.wins.append(position_value - self.stake_amount)
+                else:
+                    return
         self.balance += position_value
         trade["exited"] = True
 
-    def _exit_trades(self, current_row: pd.Series):
+    def _exit_trades(self, current_row: pd.Series, is_sl_tp: bool = False):
+        """
+        Exits All trades
+        :param current_row: Pandas row where the exit signal was generated. Must contain `close` column.
+        :param is_sl_tp: Tells the backtester to only exit trades where stop_loss or take_profit has been hit.
+        :return:
+        """
         for trade in self.trades:
-            self._exit_trade(current_row, trade)
+            self._exit_trade(current_row, trade, is_sl_tp)
 
     def run(self):
         # Loop through every row and execute trade
@@ -51,31 +85,12 @@ class BackTester:
                 self._exit_trades(row)
 
             # Check for Stop Loss and Tp Hits
-            for trade in trades:
-                if trade["exited"]:
-                    continue
-                position_value = trade["amount"] * row["close"]
-                if position_value < stake_amount:
-                    # check if stop loss has been hit
-                    loss = stake_amount - position_value
-                    pct_loss = loss / stake_amount * 100
-                    if pct_loss >= stop_loss:
-                        losses.append(loss)
-                        balance += position_value
-                        trade["exited"] = True
-                if position_value > stake_amount:
-                    # check if tp has been hit
-                    gain = position_value - stake_amount
-                    pct_gain = gain / stake_amount * 100
-                    if pct_gain >= take_profit:
-                        wins.append(gain)
-                        balance += position_value
-                        trade["exited"] = True
+            self._exit_trades(row, True)
             self.last_row = row["close"]
 
         # Liquidate Existing Trades and Update The Balance
         self._exit_trades(self.last_row)
-        win_percentage = len(wins) / total_trades * 100
+        win_percentage = len(self.wins) / total_trades * 100
         loss_percentage = len(losses) / total_trades * 100
         win_loss_ratio = len(wins) / len(losses)
         metrics = {
