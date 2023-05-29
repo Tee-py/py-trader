2061865d607db1e5665d140c2cf32485e200fd52
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-05-29 21:44:38 +0100

    completed

--- backtest.py
+++ backtest.py
@@ -6,7 +6,7 @@ class BackTester:
     def __init__(
         self, df: pd.DataFrame,
         starting_balance: int, stake_amount: int,
-        stop_loss: Optional[int], take_profit: Optional[int]
+        stop_loss: Optional[float], take_profit: Optional[float]
     ):
         self.dataframe = df
         self.balance = starting_balance
@@ -14,8 +14,12 @@ class BackTester:
         self.stop_loss = stop_loss
         self.take_profit = take_profit
         self.trades = []
+        self.last_action = None
         self.wins = []
         self.losses = []
+        self.sl_hits = 0
+        self.tp_hits = 0
+        self.force_exits = 0
         self.total_trades = 0
         self.last_row = None
 
@@ -31,6 +35,7 @@ class BackTester:
             self.trades.append({"price": current_price, "amount": amount_to_buy, "exited": False})
             self.balance -= self.stake_amount
             self.total_trades += 1
+            self.last_action = "enter_long"
 
     def _exit_trade(self, current_row: pd.Series, trade: Dict, is_sl_tp: bool = False, force_exit: bool = False):
         """
@@ -47,22 +52,32 @@ class BackTester:
         # Loosing position
         if position_value < self.stake_amount:
             if not is_sl_tp:
-                self.losses.append(self.stake_amount - position_value)
+                if force_exit:
+                    self.force_exits += 1
+                    self.losses.append(self.stake_amount - position_value)
+                else:
+                    return
             else:
                 loss = self.stake_amount - position_value
                 pct_loss = loss / self.stake_amount
                 if pct_loss >= self.stop_loss:
+                    self.sl_hits += 1
                     self.losses.append(self.stake_amount - position_value)
                 else:
                     return
         # Gaining Position
         elif position_value > self.stake_amount:
             if not is_sl_tp:
-                self.wins.append(position_value - self.stake_amount)
+                if force_exit:
+                    self.force_exits += 1
+                    self.wins.append(position_value - self.stake_amount)
+                else:
+                    return
             else:
-                gain = self.stake_amount - position_value
+                gain = position_value - self.stake_amount
                 pct_gain = gain / self.stake_amount
                 if pct_gain >= self.take_profit:
+                    self.tp_hits += 1
                     self.wins.append(position_value - self.stake_amount)
                 else:
                     return
@@ -71,8 +86,11 @@ class BackTester:
             # Exit the function if the trade is not to be exited forcefully
             if not force_exit:
                 return
+            else:
+                self.force_exits += 1
         self.balance += position_value
         trade["exited"] = True
+        self.last_action = "exit_trade"
 
     def _exit_trades(self, current_row: pd.Series, is_sl_tp: bool = False, force_exit: bool = False):
         """
@@ -98,6 +116,9 @@ class BackTester:
             "losses": no_of_loss,
             "win_amount": win_total,
             "loss_amount": loss_total,
+            "sl_hits": self.sl_hits,
+            "tp_hits": self.tp_hits,
+            "force_exits": self.force_exits,
             "win_%": win_percentage,
             "loss_%": loss_percentage,
             "w/l_ratio": win_loss_ratio,
@@ -111,10 +132,11 @@ class BackTester:
         for _, row in self.dataframe.iterrows():
             # Entry Signal
             if row["signal"] == "enter_long":
-                self._enter_long(row)
+                if self.last_action != "enter_long":
+                    self._enter_long(row)
             # Exit Signal
             if row["signal"] == "exit_long":
-                self._exit_trades(row)
+                self._exit_trades(row, force_exit=True)
             # Check for Stop Loss and Tp Hits
             self._exit_trades(row, True)
             self.last_row = row
