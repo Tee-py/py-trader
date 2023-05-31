753624abe9f7a3aa8cea8023dd5c7734cdcd7601
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-05-31 12:11:29 +0100

    added base strategy class

---
+++ strategy.py
@@ -0,0 +1,32 @@
+from abc import ABCMeta, abstractmethod
+from typing import Optional
+
+import pandas as pd
+
+
+class BaseStrategy(metaclass=ABCMeta):
+
+    # Timeframe for the strategy
+    time_frame: str = "5m"
+    # Asset to be traded by the strategy
+    asset: Optional[str] = None
+    # Stop loss for the strategy: Defaults to 1%
+    stop_loss: float = 0.01
+    # Take profit for the strategy: Defaults to 4%
+    take_profit: float = 0.04
+    # Amount of total trade-able balance to stake per trade
+    stake_amount: int = 100
+    # Allows the bot to execute short trades
+    can_short: bool = False
+
+    @abstractmethod
+    def populate_indicators(self, data_frame: pd.DataFrame):
+        raise NotImplementedError
+
+    @abstractmethod
+    def populate_entry_signal(self, data_frame: pd.DataFrame):
+        raise NotImplementedError
+
+    @abstractmethod
+    def populate_exit_signal(self, data_frame: pd.DataFrame):
+        raise NotImplementedError
